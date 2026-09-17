e4764689e9f5f3c368ea818695323f7548f442a5
Petrik <patrick160henrique@gmail.com>
2026-09-17 11:08:58 -0300

    Delete PatrickDev.LIst03.Ex01 directory

--- PatrickDev.LIst03.Ex01/src/Main.java
+++
@@ -1,9 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-
-    System.out.println("Números Inteiros de 1 á 10: ");
-    for (int i = 1; i <= 10; i++){
-        System.out.println("Número: "  + i);
-    }
-}
