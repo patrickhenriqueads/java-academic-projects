b850b721f21cce489c6f0ae00d5b07f8cdd68a5d
Petrik <patrick160henrique@gmail.com>
2026-09-17 11:17:50 -0300

    Add files via upload

---
+++ PatrickDev.LIst03/Ex02/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+
+    System.out.println("Contagem Regressiva em:");
+
+    for (int i = 10; i >= 1; i--){
+        System.out.println(i);
+    }
+    System.out.println("DECOLAR!");
+}
